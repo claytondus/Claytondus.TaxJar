dc7963a94791068eee8118befae66379c46cec55
Clayton Davis <claytondus@gmail.com>
2016-07-05 19:30:06 -0400

    Add project files.

---
+++ src/Claytondus.TaxJar/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Claytondus.TaxJar
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
